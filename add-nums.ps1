c2a2df0fe338c867ddf37385786695e20b62cf70
Kilgore_Devs <Dr.Dev.Kilgore@gmail.com>
2023-09-09 22:26:05 -0500

    add-nums.ps1 scriipt created

---
+++ add-nums.ps1
@@ -0,0 +1,11 @@
+function Add-Nums
+{
+    param([int]$num1, [int]$num2)
+    $num1 + $num2
+}
+
+$result = Add-Nums -num1 123 -num2 456
+$result += Add-Nums 1 1 1 1
+$result += Add-Nums (100-8)
+$result += Add-Nums 25
+$result
